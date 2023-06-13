9f7c2d6cd77d5d7b34cce715a2d9f0b714a264d9
Jack <127067654+JackETobin@users.noreply.github.com>
2023-06-13 00:25:16 -0400

    Add files via upload

---
+++ c_mem.c
@@ -0,0 +1,454 @@
+#include "c_mem.h"
+
+static pool_tracker _all_pools;
+
+void __pool__clear()
+{
+	if (!_all_pools.data)
+		return;
+	for (uint32 i = 0; i < _all_pools.size; i++)
+		if (*((handle_container)_all_pools.data + i))
+		{
+			free(*((handle_container)_all_pools.data + i));
+			*((handle_container)_all_pools.data + i) = NULL;
+		}
+	free(_all_pools.data);
+}
+
+void __pool__poolremove()
+{
+	if (!_all_pools.data)
+		return;
+	for (uint32 i = 0; i < _all_pools.size; i++)
+		if (!*((handle_container)_all_pools.data + i))
+		{
+			while (i++ < _all_pools.size)
+				*((handle_container)_all_pools.data + i - 1) = *((handle_container)_all_pools.data + i);
+			_all_pools.size--;
+		}
+	return;
+}
+
+uint8 __pool__close(pool_handle pool)
+{
+	if (!_all_pools.data)
+		return  _FAILURE_;
+	for (uint32 i = 0; i < _all_pools.size; i++)
+		if (*((handle_container)_all_pools.data + i) == pool)
+		{
+			free(*((handle_container)_all_pools.data + i));
+			*((handle_container)_all_pools.data + i) = NULL;
+			__pool__poolremove();
+			return _SUCCESS_;
+		}
+	return _FAILURE_;
+}
+
+void __pool__set()
+{
+	atexit(__pool__clear);
+	_all_pools.data = malloc(_POOL_TRACK_CHUNK_);
+	_all_pools.length = _POOL_TRACK_CHUNK_;
+	_all_pools.size = 0;
+}
+
+void __pool__expand()
+{
+	handle_container temp = malloc(_all_pools.length + _POOL_TRACK_CHUNK_);
+	for (int32 i = 0; i < _all_pools.size; i++)
+		*((handle_container)temp + i) = *((handle_container)_all_pools.data + i);
+	free(_all_pools.data);
+	_all_pools.data = temp;
+	_all_pools.length += _POOL_TRACK_CHUNK_;
+}
+
+void __pool__track(handle poolHandle)
+{
+	if (!_all_pools.data)
+		__pool__set();
+	if ((_all_pools.size * _SIZE_VP_) >= _all_pools.length)
+		__pool__expand();
+
+	*((handle_container)_all_pools.data + _all_pools.size) = poolHandle;
+	_all_pools.size++;
+}
+
+block_handle __pool__voidcombine(handle_container currentBlock, uint32 voidIndex)
+{
+	pool_handle onPool = ((block_handle)currentBlock)->pool;
+	handle_container freeBlock = *((handle_container)onPool->voids + voidIndex);
+	if (currentBlock < freeBlock)
+	{
+		*currentBlock = *freeBlock;
+		*(handle_container)*currentBlock = currentBlock;
+		*((handle_container)onPool->voids + voidIndex) = currentBlock;
+		return (block_handle)currentBlock;
+	}
+	else
+	{
+		*freeBlock = *currentBlock;
+		*(handle_container)*freeBlock = freeBlock;
+		return (block_handle)freeBlock;
+	}
+	return NULL;
+}
+
+uint8 __pool__voidadd(block_handle block)
+{
+	pool_handle onPool = block->pool;
+	onPool->voids -= _SIZE_VP_;
+	*(handle_container)onPool->voids = *(handle_container)block->self;
+	return _SUCCESS_;
+}
+
+uint32 __pool__voidindex(block_handle block)
+{
+	uint32 i = 0;
+	handle_container voidHandle = ((pool_handle)(block->pool))->voids;
+
+	while (((handle)voidHandle + i) < (handle)(((pool_handle)(block->pool))->next))
+	{
+		if (*((handle_container)voidHandle + i) == (handle_container)block)
+			return i;
+		i++;
+	}
+	return _FAILURE_;
+}
+
+uint8 __pool__voidremove(block_handle block)
+{
+	uint32 voidIndex = __pool__voidindex(block);
+	pool_handle onPool = block->pool;
+
+	uint32 i = voidIndex + 1;
+	while (i-- > 1)
+		*((handle_container)onPool->voids + i) = *((handle_container)onPool->voids + (i - 1));
+	onPool->voids += _SIZE_VP_;
+	return _SUCCESS_;
+}
+
+uint8 __pool__voidgenerator(handle_container voidHandle)
+{
+	uint8 flags = 0;
+	handle currentHandle;
+	
+	if ((handle)voidHandle < *voidHandle) currentHandle = voidHandle;
+	else currentHandle = *voidHandle;
+	pool_handle onPool = ((block_handle)currentHandle)->pool;
+
+	block_handle previousHandle = *((handle_container)currentHandle - 1);
+	if((handle)previousHandle > (handle)onPool && (handle)previousHandle != (handle)(onPool->voids)) flags |= (previousHandle->flags);
+	block_handle nextHandle = (*(handle_container)currentHandle) + _SIZE_VP_;
+	if((handle)nextHandle < (handle)(onPool->voids)) flags |= (nextHandle->flags << 1);
+
+	switch (flags)
+	{
+	case _LOWER_FREE_:
+		__pool__voidcombine((handle_container)currentHandle, __pool__voidindex(previousHandle));
+		break;
+	case _UPPER_FREE_:
+		__pool__voidcombine((handle_container)currentHandle, __pool__voidindex(nextHandle));
+		break;
+	case _BOTH_FREE_:
+		__pool__voidremove(__pool__voidcombine((handle_container)previousHandle, __pool__voidindex(nextHandle)));
+		break;
+	case _NONE_FREE_:
+		__pool__voidadd((block_handle)currentHandle);
+		break;
+	default:
+		return _FAILURE_;
+	}
+	return _SUCCESS_;
+}
+
+uint64 __pool__voidsize(handle_container voidHandle)
+{
+	int64 voidSize = *(handle_container)voidHandle - (handle)voidHandle;
+	if (voidSize < 0) voidSize *= -1;
+	return (uint64)voidSize + _SIZE_VP_;
+}
+
+struct _sv_props __pool__voidprops(handle_container voidHandle)
+{
+	struct _sv_props voidProperties = { 0 };
+	if (!*voidHandle)
+		return voidProperties;
+	voidProperties.voidPtr = voidHandle;
+	voidProperties.voidSize = __pool__voidsize(voidHandle);
+
+	return voidProperties;
+}
+
+uint8 __pool__voidshrink(struct _sv_props voidProps, uint64 size)
+{
+	if (size >= voidProps.voidSize)
+		return _FAILURE_;
+	
+	uint32 voidIndex = __pool__voidindex(voidProps.voidPtr);
+	pool_handle onPool = ((block_handle)voidProps.voidPtr)->pool;
+	handle voidBegin = *((handle_container)onPool->voids + voidIndex);
+	handle voidEnd = *(handle_container)*((handle_container)onPool->voids + voidIndex);
+
+	voidBegin += size;
+	((block_handle)(voidBegin))->flags |= _BLOCK_FREE_;
+	((block_handle)(voidBegin))->pool = onPool;
+	*((handle_container)onPool->voids + voidIndex) = voidBegin;
+	*(handle_container)* ((handle_container)onPool->voids + voidIndex) = voidEnd;
+
+	return _SUCCESS_;
+}
+
+block_handle __pool__voidfill(pool_handle pool, uint64 size)
+{
+	struct _sv_props voidProps = { 0 };
+	for (uint32 i = 0; (handle)pool->voids + i < (handle)pool->next; i++)
+	{
+		voidProps = __pool__voidprops(*((handle_container)pool->voids + i));
+		if (voidProps.voidSize >= size)
+			break;
+		voidProps.voidPtr = 0;
+	}
+	if (voidProps.voidSize == size || voidProps.voidSize - size < _SIZE_BLOCK_MIN_)
+		__pool__voidremove((block_handle)voidProps.voidPtr);
+	else
+		__pool__voidshrink(voidProps, size);
+	return voidProps.voidPtr;
+}
+
+pool_handle OpenPool(uint32 sizeMB)
+{
+	uint64 sizeBytes = _SIZE_MB_ * sizeMB;
+	uint64 zeroIterator = sizeBytes / _SIZE_INT64_;
+
+	handle poolHandle = malloc(sizeBytes);
+	while (zeroIterator--)
+		*((uint64*)poolHandle + zeroIterator) = 0;
+	
+	struct _pool temp;
+	temp.lenMB = sizeMB;
+	temp.data = poolHandle + sizeof(struct _pool);
+	temp.voids = temp.next = poolHandle + sizeBytes - _SIZE_VP_;
+	*(handle_container)temp.next = (handle)temp.data;
+	
+	*(pool_handle)poolHandle = temp;
+	__pool__track(poolHandle);
+	
+	return (pool_handle)poolHandle;
+}
+
+uint8 ClosePool(pool_handle* pool)
+{
+	__pool__close(*pool);
+	*pool = NULL;
+	return 0;
+}
+
+void_info PoolVoidInfo(pool_handle pool)
+{
+	uint64 totalSpace = 0;
+	void_info voidInfo = { 0 };	
+	struct _sv_props largestVoid = { 0 };
+
+	if (pool->voids == pool->next)
+		return voidInfo;
+
+	voidInfo.numVoids = (pool->next - pool->voids) / _SIZE_VP_;
+	for (uint32 i = 0; i < voidInfo.numVoids; i++)
+	{
+		totalSpace += __pool__voidsize(*((handle_container)pool->voids + i));
+		if (__pool__voidsize(*((handle_container)pool->voids + i)) > largestVoid.voidSize)
+			largestVoid = __pool__voidprops(*((handle_container)pool->voids + i));
+	}
+	voidInfo.totalSpace = totalSpace;
+	voidInfo.largestVoid = largestVoid;
+
+	return voidInfo;
+}
+
+block_handle BuildBlock(pool_handle pool, uint32 numElements, uint64 eleSizeBytes)
+{
+	uint64 blockSize = sizeof(struct _block) + (numElements * _SIZE_VP_) + (numElements * eleSizeBytes) + _SIZE_VP_;
+	block_handle temp = __pool__voidfill(pool, blockSize);
+	if (!temp)
+	{
+		temp = *(handle_container)pool->next;
+		*(handle_container)pool->next += blockSize;
+	}
+	handle selfPtr = (handle)&temp->elements + ((numElements + 1) * _SIZE_VP_) + (numElements * eleSizeBytes);
+	handle dataPtr = (handle)&temp->elements + ((numElements + 1) * _SIZE_VP_);
+
+	temp->self = selfPtr;
+	*(handle_container)temp->self = &temp->self;
+	temp->flags = 0;
+	temp->pool = pool;
+	temp->elements = dataPtr;
+	for (uint32 i = 1; i <= numElements; i++)
+		*(&temp->elements + i) = dataPtr + ((i - 1) * eleSizeBytes);
+
+	return temp;
+}
+
+uint8 FreeBlock(block_handle* block)
+{
+	__pool__voidgenerator((handle_container)*block);
+	(*block)->flags |= _BLOCK_FREE_;
+	*block = NULL;
+	return 0;
+}
+
+uint64 BlockSize(block_handle block)
+{
+	uint64 blockSize = (handle)block->self - (handle)block + _SIZE_VP_;
+	return blockSize;
+}
+
+uint64 ElementSize(block_handle block)
+{
+	uint64 elementSize = (handle)block->self - *(handle_container)(&block->elements + NumElements(block));
+	return elementSize;
+}
+
+uint32 NumElements(block_handle block)
+{
+	uint32 numElements = ((void*)block->elements - (void*)(&block->elements) - _SIZE_VP_) / _SIZE_VP_;
+	return numElements;
+}
+
+handle GetElementAt(block_handle block, uint32 element)
+{
+	if (element < 1 || element > NumElements(block))
+		return NULL;
+	handle elementHandle = *(handle_container)(&block->elements + element);
+	return elementHandle;
+}
+
+handle GetData(block_handle block)
+{
+	return block->elements;
+}
+
+uint8 Push(block_handle block, handle data)
+{
+	uint64 elementSize = ElementSize(block);
+	handle shiftStart = **(handle_container*)block->self;
+
+	uint64 i = 0;
+	while ((shiftStart - elementSize - i++ > block->elements))
+		*((uint8*)shiftStart - i) = *((uint8*)shiftStart - elementSize - i);
+
+	for (i = 0; i < elementSize; i++)
+		*((uint8*)block->elements + i) = *((uint8*)data + i);
+
+	return _SUCCESS_;
+}
+
+uint8 Pop(block_handle block)
+{
+	uint64 elementSize = ElementSize(block);
+	handle shiftStop = **(handle_container*)block->self;
+
+	uint64 i = 0;
+	while ((block->elements + elementSize + i < shiftStop))
+	{
+		*((uint8*)block->elements + i) = *((uint8*)block->elements + elementSize + i);
+		i++;
+	}
+	i -= elementSize;
+	while((uint8*)block->elements + (i++) < (uint8*)shiftStop - 1)
+		*((uint8*)block->elements + i) = 0;
+
+	return _SUCCESS_;
+}
+
+uint8 Set(block_handle block, handle data, uint32 position)
+{
+	if (position < 1 || position > NumElements(block))
+		return _FAILURE_;
+	uint64 elementSize = ElementSize(block);
+	for (uint64 i = 0; i < elementSize; i++)
+		*(uint8*)(*(&block->elements + position) + i) = *((uint8*)data + i);
+	return 0;
+}
+
+uint8 ZeroElement(block_handle block, uint32 position)
+{
+	if (position < 1 || position > NumElements(block))
+		return _FAILURE_;
+	uint64 elementSize = ElementSize(block);
+	for (uint64 i = 0; i < elementSize; i++)
+		*(uint8*)(*(&block->elements + position) + i) = 0;
+	return 0;
+}
+
+uint8 ZeroBlock(block_handle block)
+{
+	uint64 elementSize = ElementSize(block);
+	handle shiftStop = **(handle_container*)block->self;
+
+	uint64 i = 0;
+	while ((block->elements + i < shiftStop))
+	{
+		*((uint8*)block->elements + i) = 0;
+		i++;
+	}
+	return _SUCCESS_;
+}
+
+uint8 RotateFirst(block_handle block)
+{
+	pool_handle onPool = block->pool;
+	uint64 elementSize = ElementSize(block);
+	if ((onPool->voids - *(handle_container)onPool->next) < elementSize)
+		return _FAILURE_;
+	
+	uint64 i;
+	handle shiftStop = **(handle_container*)block->self;
+
+	for (i = 0; i < elementSize; i++)
+		*((uint8*)(*(handle_container)onPool->next) + i) = *((uint8*)block->elements + i);
+
+	i = 0;
+	while ((block->elements + elementSize + i < shiftStop))
+	{
+		*((uint8*)block->elements + i) = *((uint8*)block->elements + elementSize + i);
+		i++;
+	}
+
+	uint64 iter = 0;
+	while ((uint8*)block->elements + i < (uint8*)shiftStop - 1)
+	{
+		*((uint8*)block->elements + i) = *((uint8*)(*(handle_container)onPool->next) + iter);
+		i++, iter++;
+	}
+
+	for (i = 0; i < elementSize; i++)
+		*((uint8*)(*(handle_container)onPool->next) + i) = 0;
+
+	return _SUCCESS_;
+}
+
+uint8 RotateLast(block_handle block)
+{
+	pool_handle onPool = block->pool;
+	uint64 elementSize = ElementSize(block);
+	if ((onPool->voids - *(handle_container)onPool->next) < elementSize)
+		return _FAILURE_;
+
+	uint64 i;
+	handle shiftStart = **(handle_container*)block->self;
+
+	for (i = 0; i < elementSize; i++)
+		*((uint8*)(*(handle_container)onPool->next) + i) = *((uint8*)shiftStart - elementSize + i);
+
+	i = 0;
+	while ((shiftStart - elementSize - i++ > block->elements))
+		*((uint8*)shiftStart - i) = *((uint8*)shiftStart - elementSize - i);
+
+	for (i = 0; i < elementSize; i++)
+		*((uint8*)block->elements + i) = *((uint8*)(*(handle_container)onPool->next) + i);
+
+	for (i = 0; i < elementSize; i++)
+		*((uint8*)(*(handle_container)onPool->next) + i) = 0;
+
+	return _SUCCESS_;
+}
---
+++ c_mem.h
@@ -0,0 +1,106 @@
+#ifndef C_MEM_H
+#define C_MEM_H
+
+#include <stdlib.h>
+
+#ifndef _TYPES_
+#define _TYPES_
+
+typedef char				int8;
+typedef unsigned char		uint8;
+
+typedef short				int16;
+typedef unsigned short		uint16;
+
+typedef int					int32;
+typedef unsigned int		uint32;
+
+typedef long long			int64;
+typedef unsigned long long	uint64;
+
+#endif
+#ifndef NULL
+#define NULL ((void*)0)
+#endif
+
+#define _SUCCESS_ 1
+#define _FAILURE_ 0
+
+#define _SIZE_VP_ sizeof(void*)
+#define _SIZE_KB_ 1024
+#define _SIZE_MB_ (1024 * 1024)
+#define _SIZE_INT8_ sizeof(char);
+#define _SIZE_INT64_ sizeof(int64)
+
+#define _POOL_TRACK_CHUNK_ 24
+
+#define _BLOCK_FREE_ (1 << 0)
+
+#define _LOWER_FREE_ 1
+#define _UPPER_FREE_ 2
+#define _BOTH_FREE_ 3
+#define _NONE_FREE_ 0
+
+typedef void* handle;
+typedef void** handle_container;
+
+typedef struct _pool
+{
+	uint32 lenMB;
+	handle data;
+	handle next;
+	handle voids;
+} *pool_handle;
+
+typedef struct _pool_tracker
+{
+	handle_container	data;
+	uint32				length;
+	uint32				size;
+}pool_tracker;
+
+struct _sv_props
+{
+	uint64				voidSize;
+	handle				voidPtr;
+};
+
+typedef struct _pool_void_properties
+{
+	uint32				numVoids;
+	uint64				totalSpace;
+	struct _sv_props	largestVoid;
+}void_info, *void_info_handle;
+
+typedef struct _block
+{
+	handle_container	self;
+	uint8				flags;
+	pool_handle			pool;	
+	handle				elements;
+} *block_handle;
+#define _SIZE_BLOCK_MIN_ sizeof(struct _block) + (4 * _SIZE_VP_)
+
+pool_handle OpenPool(uint32 sizeMB);
+uint8 ClosePool(pool_handle* pool);
+void_info PoolVoidInfo(pool_handle pool);
+
+block_handle BuildBlock(pool_handle pool, uint32 numElements, uint64 elementSizeByes);
+uint8 FreeBlock(block_handle* block);
+
+uint64 BlockSize(block_handle block);
+uint64 ElementSize(block_handle block);
+uint32 NumElements(block_handle block);
+handle GetElementAt(block_handle block, uint32 element);
+
+uint8 Push(block_handle block, handle data);
+uint8 Pop(block_handle block);
+uint8 Set(block_handle block, handle data, uint32 position);
+
+uint8 ZeroElement(block_handle block, uint32 position);
+uint8 ZeroBlock(block_handle block);
+
+uint8 RotateFirst(block_handle block);
+uint8 RotateLast(block_handle block);
+
+#endif
